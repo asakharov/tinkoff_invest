2ca53983c321d2e64b6fbea970fdcbfb48fd21a4
asakharov <alexey.sakharov@gmail.com>
2021-09-12 14:21:31 +0300

    new: add default config for the client

---
+++ config.py
@@ -0,0 +1,4 @@
+SANDBOX_SERVER = 'https://api-invest.tinkoff.ru/openapi/sandbox/'
+PRODUCTION_SERVER = 'https://api-invest.tinkoff.ru/openapi/'
+WEB_SOCKETS_SERVER = 'wss://api-invest.tinkoff.ru/openapi/md/v1/md-openapi/ws'
+EVENTS_PROCESSING_WORKERS_COUNT = 5
