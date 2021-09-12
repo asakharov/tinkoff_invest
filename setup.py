7211c165c3f383abe95e974d5db70a31f8a776c0
asakharov <alexey.sakharov@gmail.com>
2021-09-12 14:21:50 +0300

    new: add package requirements

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
     version='1.0.0',
     packages=['tinkoff_invest', 'tinkoff_invest.models'],
     url='https://github.com/asakharov/tinkoff_invest',
+    python_requires='>=3.8.0',
+    install_requires=[
+        "prettytable", "iso8601", "requests", "ujson", "urllib3", "websocket-client"
+    ],
     license='MIT',
     author='Alexey Sakharov',
     author_email='alexey.sakharov@gmail.com',
